263456d2f3e0e3b050d0b6ba3de008dc54b4718b
salahuddin09 <salahuddincse09@gmail.com>
2022-04-27 12:53:31 +0600

    fixed rank issue by used id

--- src/main/java/com/userstore/repositories/UserProgressRepository.java
+++ src/main/java/com/userstore/repositories/UserProgressRepository.java
@@ -20,16 +20,18 @@ public interface UserProgressRepository extends CrudRepository<UserProgress, Lon
 			, nativeQuery = true)
 	List<LeaderBoardView> findUserListByScore(@Param("limit") Integer limit);
 	
-	@Query(value = " SELECT RANK() OVER ("
-			+ " ORDER BY userprogress_tbl.score DESC"
-			+ " ) AS rank, "
-			+ "  userprogress_tbl.level AS level,"
-			+ " (SELECT users_tbl.name FROM users_tbl WHERE users_tbl.progress_id = userprogress_tbl.id) AS name,"
-			+ " userprogress_tbl.score AS score "
-			+ " FROM userprogress_tbl  "
-			+ " inner join users_tbl "
-			+ " ON users_tbl.progress_id = userprogress_tbl.id "
-			+ " WHERE users_tbl.id = :userId " 
+	@Query(value = " SELECT rank, level, name,score,user_id "
+			+ " FROM "
+			+ " ( "
+			+ "  SELECT RANK () OVER (ORDER BY userprogress_tbl.score DESC) AS rank, "
+			+ "	userprogress_tbl.level AS level, "
+			+ "	(SELECT users_tbl.name FROM users_tbl "
+			+ "	 WHERE users_tbl.progress_id = userprogress_tbl.id) AS name, "
+			+ "	 userprogress_tbl.score AS score, "
+			+ "	(SELECT users_tbl.id FROM users_tbl "
+			+ "	 WHERE users_tbl.progress_id = userprogress_tbl.id) AS user_id FROM userprogress_tbl "
+			+ " ) all_users "
+			+ " WHERE user_id = :userId " 
 			, nativeQuery = true)
 	LeaderBoardView findUserScoreByUserId(@Param("userId") Long userId);
 	
